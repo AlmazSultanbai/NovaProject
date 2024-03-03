9e5af51f629027bc6325675979b1dff64f6d8352
Sultanbai Almaz <aldeco312@gmail.com>
2024-03-03 09:07:06 +0600

    Initial Commit

---
+++ NovaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NovaProject
+//
+//  Created by Sultanbai Almaz on 3/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
